52b20af8d023644fdce798c5bc94b43937add919
KamiKruse <altsake@protonmail.com>
2024-11-15 17:25:56 +0530

    Add split images and links from text node and add associated tests

---
+++ src/split_images_links.py
@@ -0,0 +1,58 @@
+from textnode import TextNode,TextType
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+def split_nodes_image(old_nodes):
+    split_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        current_type = node.text_type
+        if current_text == "":
+            continue
+        if current_type == TextType.TEXT:
+            image_split = extract_markdown_images(current_text)
+            start = 0
+            image_index = 0
+            while image_index < len(image_split):
+                open_pos = current_text.find(image_split[image_index][0], start)
+                if open_pos == -1:
+                    break
+                text_to_append = current_text[start:open_pos-2]
+                if open_pos != 0 and text_to_append:
+                    split_nodes.append(TextNode(current_text[start:open_pos-2], TextType.TEXT))
+                close_pos = open_pos + len(image_split[image_index][0]) + len(image_split[image_index][1]) + 3
+                split_nodes.append(TextNode(image_split[image_index][0], TextType.IMAGE, url=image_split[image_index][1]))
+                start = close_pos
+                image_index = image_index + 1
+            if current_text[start:]:
+                split_nodes.append(TextNode(current_text[start:], TextType.TEXT))
+        else:
+            split_nodes.append(TextNode(current_text, current_type))
+    return split_nodes
+                
+def split_nodes_links(old_nodes):
+    split_nodes = []
+    for node in old_nodes:
+        current_text = node.text
+        current_type = node.text_type
+        if current_text == "":
+            continue
+        if current_type == TextType.TEXT:
+            link_split = extract_markdown_links(current_text)
+            start = 0
+            link_index = 0
+            while link_index < len(link_split):
+                open_pos = current_text.find(link_split[link_index][0], start)
+                if open_pos == -1:
+                    break
+                text_to_append = current_text[start:open_pos-1] 
+                if open_pos != 0 and text_to_append :
+                    split_nodes.append(TextNode(current_text[start:open_pos-1], TextType.TEXT))
+                close_pos = open_pos + len(link_split[link_index][0]) + len(link_split[link_index][1]) + 3
+                split_nodes.append(TextNode(link_split[link_index][0], TextType.LINK, url=link_split[link_index][1]))
+                start = close_pos
+                link_index = link_index + 1
+            if current_text[start:]:
+                split_nodes.append(TextNode(current_text[start:], TextType.TEXT))
+        else:
+            split_nodes.append(TextNode(current_text, current_type))
+    return split_nodes
---
+++ src/test_split_images_links.py
@@ -0,0 +1,60 @@
+import unittest
+from split_images_links import split_nodes_links, split_nodes_image
+from textnode import TextNode, TextType
+
+class TestSplitImages(unittest.TestCase):
+    def test_split_nodes_links_basic(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        nodes = split_nodes_links([node])
+        self.assertEqual(len(nodes), 4)
+        self.assertEqual(nodes[0].text, "This is text with a link ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "to boot dev")
+        self.assertEqual(nodes[1].text_type, TextType.LINK)
+        self.assertEqual(nodes[1].url, "https://www.boot.dev")
+        self.assertEqual(nodes[2].text, " and ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "to youtube")
+        self.assertEqual(nodes[3].text_type, TextType.LINK)
+
+    def test_split_nodes_images_basic(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(len(nodes), 4)
+        self.assertEqual(nodes[0].text, "This is text with a ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "rick roll")
+        self.assertEqual(nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(nodes[1].url, "https://i.imgur.com/aKaOqIh.gif")
+        self.assertEqual(nodes[2].text, " and ")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(nodes[3].text, "obi wan")
+        self.assertEqual(nodes[3].text_type, TextType.IMAGE)
+
+    def test_split_nodes_images_only(self):
+        node = TextNode("![rick roll](https://i.imgur.com/aKaOqIh.gif)",TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].text, "rick roll")
+        self.assertEqual(nodes[0].text_type, TextType.IMAGE)
+
+    def test_split_nodes_links_only(self):
+        node = TextNode("[to boot dev](https://www.boot.dev)",TextType.TEXT)
+        nodes = split_nodes_links([node])
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].text, "to boot dev")
+        self.assertEqual(nodes[0].text_type, TextType.LINK)
+
+    def test_split_nodes_links_only(self):
+        node = TextNode("",TextType.TEXT)
+        nodes = split_nodes_links([node])
+        self.assertEqual(len(nodes), 0)
+
+    def test_split_nodes_images_only(self):
+        node = TextNode("",TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(len(nodes), 0)
+        
+
+if __name__ == "__main__":
+    unittest.main()
